e65c7c427e779e2d1d7ab2385b9fbfc253c65ef1
bwp334 <bwp334@BWP334.local>
2025-01-28 09:16:07 +0900

    修正

--- query/バス判定用クエリ.sql
+++ query/バス判定用クエリ.sql
@@ -70,21 +70,23 @@ AS (
 
 
 #まず車と自転車と予測したセグメントをログごとに分割する
-WITH table_car_or_bike AS (
+create or replace table `prd-analysis.w_t_akiyama.bus_output_sample_fix` as
+with
+table_car_or_bike AS (
   SELECT
     common_id, seg_no, is_uuid, os, arrive_ptime, depart_time, visiting_seconds, cnt, mesh, is_stay,
-    raw_element.sdk_detect_ptime, raw_element.latitude, raw_element.longitude, raw_element.accuracy,
+    raw_element.sdk_detect_ptime, raw_element.latitude_anonymous, raw_element.longitude_anonymous, raw_element.accuracy,
     needs_tokuminer_flg, pred_tpmode, purpose_flg
-  FROM `input_sample` AS st
+  FROM `prd-analysis.w_t_akiyama.bus_input_sample` AS st
   CROSS JOIN UNNEST(st.raw_array) AS raw_element
   WHERE (pred_tpmode = "car" OR pred_tpmode = "bike")
-    AND raw_element.latitude BETWEEN -90 AND 90
-    AND raw_element.longitude BETWEEN -180 AND 180
+    AND raw_element.latitude_anonymous BETWEEN -90 AND 90
+    AND raw_element.longitude_anonymous BETWEEN -180 AND 180
 ),
 
 table_not_car_bike AS (
   SELECT *
-  FROM `input_sample`
+  FROM `prd-analysis.w_t_akiyama.bus_input_sample`
   WHERE pred_tpmode != "car" AND pred_tpmode != "bike"
 ),
 
@@ -93,13 +95,13 @@ dummy_probe_joined AS (
   SELECT
     bus.*,
     -- mesh.mt_mesh など必要なら使える
-  FROM `ダミープローブデータ` bus
-  JOIN `長野県メッシュデータ` mesh
+  FROM `prd-analysis.w_t_akiyama.dummy_probe_data_for_git` bus
+  JOIN `prd-analysis.w_t_akiyama.nagano_mesh_table` mesh
     ON mesh.mt_mesh = CAST(convert_to_mesh(bus.lat, bus.lon) AS INT64)
   WHERE
     bus.lat IS NOT NULL
     AND bus.lon IS NOT NULL
-    AND DATE(DATETIME(bus.date_time, "Asia/Tokyo")) BETWEEN "2024-01-01" AND "2024-01-14"
+    AND DATE(TIMESTAMP(bus.date_time, "Asia/Tokyo")) BETWEEN "2024-01-01" AND "2024-01-14"
 ),
 
 /* ★ ここで table_car_or_bike に LEFT JOIN して busフラグを付与 */
@@ -109,14 +111,14 @@ bus_flg_table AS (
     CASE WHEN bus.id IS NULL THEN 0 ELSE 1 END AS bus_flg
   FROM table_car_or_bike
   LEFT JOIN dummy_probe_joined AS bus
-    ON convert_to_mesh_9(bus.lat, bus.lon) = convert_to_mesh_9(table_car_or_bike.latitude, table_car_or_bike.longitude)
+    ON convert_to_mesh_9(bus.lat, bus.lon) = convert_to_mesh_9(table_car_or_bike.latitude_anonymous, table_car_or_bike.longitude_anonymous)
     AND DATE(table_car_or_bike.sdk_detect_ptime, "Asia/Tokyo")
-         = DATE(DATETIME(bus.date_time, "Asia/Tokyo"))
+         = DATE(TIMESTAMP(bus.date_time, "Asia/Tokyo"))
     AND table_car_or_bike.sdk_detect_ptime BETWEEN
-      TIMESTAMP(DATETIME_SUB(DATETIME(bus.date_time, "Asia/Tokyo"), INTERVAL 30 SECOND))
-      AND TIMESTAMP(DATETIME_ADD(DATETIME(bus.date_time, "Asia/Tokyo"), INTERVAL 29 SECOND))
+      TIMESTAMP(DATETIME_SUB(TIMESTAMP(bus.date_time, "Asia/Tokyo"), INTERVAL 30 SECOND))
+      AND TIMESTAMP(DATETIME_ADD(TIMESTAMP(bus.date_time, "Asia/Tokyo"), INTERVAL 29 SECOND))
     AND ST_DWITHIN(
-      ST_GEOGPOINT(table_car_or_bike.longitude, table_car_or_bike.latitude),
+      ST_GEOGPOINT(table_car_or_bike.longitude_anonymous, table_car_or_bike.latitude_anonymous),
       ST_GEOGPOINT(bus.lon, bus.lat),
       LEAST(table_car_or_bike.accuracy, 300)
     )
@@ -125,13 +127,13 @@ bus_flg_table AS (
 /* 以下、元のロジックと同じ */
 bus_flg_cnt_1 AS (
   SELECT
-    common_id, seg_no, is_uuid, os, sdk_detect_ptime, latitude, longitude,
+    common_id, seg_no, is_uuid, os, sdk_detect_ptime, latitude_anonymous, longitude_anonymous,
     arrive_ptime, depart_time, visiting_seconds, accuracy, cnt, mesh,
     is_stay, needs_tokuminer_flg, pred_tpmode, purpose_flg,
     MAX(bus_flg) AS bus_flg
   FROM bus_flg_table
   GROUP BY
-    common_id, seg_no, is_uuid, os, sdk_detect_ptime, latitude, longitude,
+    common_id, seg_no, is_uuid, os, sdk_detect_ptime, latitude_anonymous, longitude_anonymous,
     arrive_ptime, depart_time, visiting_seconds, accuracy, cnt, mesh,
     is_stay, needs_tokuminer_flg, pred_tpmode, purpose_flg
 ),
@@ -141,9 +143,9 @@ bus_flg_table_v2 AS (
     bft.*,
     CASE WHEN poly.polygon IS NULL THEN 0 ELSE 1 END AS bus_flg_2
   FROM bus_flg_cnt_1 AS bft
-  LEFT JOIN `長野県バスルートデータ` AS poly
+  LEFT JOIN `prd-analysis.w_t_akiyama.bus_polygon_matsumoto_sample_json` AS poly
     ON ST_CONTAINS(ST_GEOGFROMTEXT(poly.polygon),
-                   ST_GEOGPOINT(bft.longitude, bft.latitude))
+                   ST_GEOGPOINT(bft.longitude_anonymous, bft.latitude_anonymous))
 ),
 bus_flg_cnt_2 AS (
   SELECT
@@ -152,8 +154,8 @@ bus_flg_cnt_2 AS (
     is_uuid,
     os,
     sdk_detect_ptime,
-    latitude,
-    longitude,
+    latitude_anonymous,
+    longitude_anonymous,
     arrive_ptime,
     depart_time,
     visiting_seconds,
@@ -168,8 +170,8 @@ bus_flg_cnt_2 AS (
     MAX(bus_flg_2) AS bus_flg_2
   FROM bus_flg_table_v2
   GROUP BY
-    common_id, seg_no, is_uuid, os, sdk_detect_ptime, latitude, longitude,
-    arrive_ptime, depart_time, visiting_seconds, accuracy, cnt, mesh,
+    common_id, seg_no, is_uuid, os, sdk_detect_ptime, latitude_anonymous, longitude_anonymous,
+    arrive_ptime, depart_time, visiting_seconds, accuracy,cnt, mesh,
     is_stay, needs_tokuminer_flg, pred_tpmode, purpose_flg, bus_flg
 ),
 bus_flg_sum AS (
@@ -185,7 +187,7 @@ bus_cnt_table AS (
   SELECT bf.*, b_s.bus_cnt, b_s.bus_cnt_2
   FROM (
     SELECT *
-    FROM `input_sample`
+    FROM `prd-analysis.w_t_akiyama.bus_input_sample`
     WHERE pred_tpmode="car" OR pred_tpmode="bike"
   ) AS bf
   LEFT JOIN bus_flg_sum AS b_s
@@ -208,14 +210,14 @@ move_table AS (
 table_car_v2 AS (
   SELECT
     common_id, seg_no, is_uuid, os, arrive_ptime, depart_time, visiting_seconds,
-    cnt, mesh, is_stay, raw_element.sdk_detect_ptime, raw_element.latitude,
-    raw_element.longitude, raw_element.accuracy, 
+    cnt, mesh, is_stay, raw_element.sdk_detect_ptime, raw_element.latitude_anonymous,
+    raw_element.longitude_anonymous, raw_element.accuracy,
     needs_tokuminer_flg, pred_tpmode, purpose_flg
   FROM move_table AS st
   CROSS JOIN UNNEST(st.raw_array) AS raw_element
   WHERE (pred_tpmode = "car" OR pred_tpmode = "bike")
-    AND raw_element.latitude BETWEEN -90 AND 90
-    AND raw_element.longitude BETWEEN -180 AND 180
+    AND raw_element.latitude_anonymous BETWEEN -90 AND 90
+    AND raw_element.longitude_anonymous BETWEEN -180 AND 180
 ),
 group_car_table AS (
   SELECT
@@ -223,15 +225,15 @@ group_car_table AS (
     mv2.common_id AS nearby_id
   FROM table_car_v2 AS mv
   LEFT JOIN table_car_v2 AS mv2
-    ON convert_to_mesh_9(mv.latitude, mv.longitude)
-       = convert_to_mesh_9(mv2.latitude, mv2.longitude)
+    ON convert_to_mesh_9(mv.latitude_anonymous, mv.longitude_anonymous)
+       = convert_to_mesh_9(mv2.latitude_anonymous, mv2.longitude_anonymous)
     AND DATE(mv.sdk_detect_ptime, "Asia/Tokyo")
         = DATE(mv2.sdk_detect_ptime, "Asia/Tokyo")
     AND TIMESTAMP_DIFF(mv.sdk_detect_ptime, mv2.sdk_detect_ptime, SECOND)
         BETWEEN -29 AND 30
     AND ST_DWithin(
-      ST_GeogPoint(mv.longitude, mv.latitude),
-      ST_GeogPoint(mv2.longitude, mv2.latitude),
+      ST_GeogPoint(mv.longitude_anonymous, mv.latitude_anonymous),
+      ST_GeogPoint(mv2.longitude_anonymous, mv2.latitude_anonymous),
       LEAST(mv.accuracy, 100)
     )
     AND mv.common_id != mv2.common_id
